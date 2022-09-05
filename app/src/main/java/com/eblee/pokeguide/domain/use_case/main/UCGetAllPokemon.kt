75bc9d7757b52eb027966c775b4495f7de7bc4d0
EUNGB <dldmsql114@gmail.com>
2022-09-06 01:42:13 +0900

    포켓몬 상세 조회 rx 연습

--- app/src/main/java/com/eblee/pokeguide/domain/use_case/main/UCGetAllPokemon.kt
+++ app/src/main/java/com/eblee/pokeguide/domain/use_case/main/UCGetAllPokemon.kt
@@ -1,11 +1,19 @@
 package com.eblee.pokeguide.domain.use_case.main
 
 import android.annotation.SuppressLint
+import android.util.Log
+import com.eblee.pokeguide.data.remote.entity.PokemonEntity
 import com.eblee.pokeguide.domain.entity.Pokemon
+import com.eblee.pokeguide.domain.entity.PokemonInfo
 import com.eblee.pokeguide.domain.repository.PokemonLocalRepository
 import com.eblee.pokeguide.domain.repository.PokemonRepository
+import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class UCGetAllPokemon(
     private val pokemonRepository: PokemonRepository,
@@ -14,6 +22,21 @@ class UCGetAllPokemon(
 
     @SuppressLint("CheckResult")
     fun invoke(): Single<List<Pokemon>> {
+      /*  val request: ArrayList<Single<PokemonInfo>> = ArrayList()
+        for (id in 1..20) {
+            request.add(pokemonRepository.getPokemonInfo(id).subscribeOn(Schedulers.io()))
+        }
+        Single.zip(request) { it ->
+            it.toList() as List<PokemonInfo>
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TAG", "invoke: ${it.toString()}")
+            }) {
+                it.printStackTrace()
+            }*/
+
         return Single.zip(
             pokemonRepository.getAllPokemon().subscribeOn(Schedulers.io()),
             pokemonLocalRepository.getPokemonNames().subscribeOn(Schedulers.io()),
